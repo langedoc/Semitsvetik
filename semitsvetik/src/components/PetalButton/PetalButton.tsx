ca2f101838789122974a6f4dbbf77a58d39c529e
yulia <langedoc@gmail.com>
2024-09-29 18:38:45 +0200

    style: adding dinamically background color to petals

--- semitsvetik/src/components/PetalButton/PetalButton.tsx
+++ semitsvetik/src/components/PetalButton/PetalButton.tsx
@@ -2,11 +2,12 @@ import style from './PetalButton.module.css';
 
 interface PetalButtonProps {
     text: string;
+    backgroundColor?: string;
 }
 
-export default function PetalButton({text}: PetalButtonProps) {
+export default function PetalButton({text, backgroundColor}: PetalButtonProps) {
     return (
-        <button className={style.petalButton}>
+        <button className={style.petalButton} style={{backgroundColor}}>
             <span>{text}</span>
         </button>
     )
