ca488294f48eb6bfe2284ec85ceca8041ae3ab49
yulia <langedoc@gmail.com>
2024-10-14 13:18:55 +0200

    feat: events top section

--- semitsvetik/src/pages/Events/Events.tsx
+++ semitsvetik/src/pages/Events/Events.tsx
@@ -3,12 +3,17 @@ import style from './Events.module.css';
 export default function Events() {
     return (
         <div className={style.container}>
-            <h1>Мероприятия</h1>
-            <ul>
-                <li>Книгоцветик</li>
-                <li>Киноцветик</li>
-                <li>Театральный цветик</li>
-            </ul>
+            <div className={style.topInfo}>
+                <h1>Мероприятия</h1>
+                <section className={style.schedule}>
+                    <h3>БЛИЖАЙШЕЕ:</h3>
+                    <p><b>Event title</b></p>
+                    <p> day/month/year - <b> time </b></p>
+                    <p>place</p>
+                </section>
+            </div>
+            <div className={style.content}>
+            </div>
         </div>
     );
 };
