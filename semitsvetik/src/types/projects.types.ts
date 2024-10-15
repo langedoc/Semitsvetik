4177074e9d8be9e2d5621d5334601a3863cbf18f
yulia <langedoc@gmail.com>
2024-10-15 15:44:21 +0200

    feat: projectDetails component

--- semitsvetik/src/types/projects.types.ts
+++ semitsvetik/src/types/projects.types.ts
@@ -1,5 +1,11 @@
 export interface ProjectProps {
     title: string;
     imageSrc?: string;
-    description?: string;
+    description: string;
+    longDescription?: string;
+    place?: string;
+    placeLink?: string;
+    contacts?: string;
+    infoLink?: string;
+    photos?: string[];
 }
