9debca4379a9439f1e4302a8982a25e7be53de98
yulia <langedoc@gmail.com>
2024-10-16 12:03:55 +0200

    style: contact page styling

--- semitsvetik/src/pages/Contact/Contact.tsx
+++ semitsvetik/src/pages/Contact/Contact.tsx
@@ -16,7 +16,7 @@ export default function Contact() {
             </div>
             <div className={style.content}>
                 <h3>Как с нами связаться</h3>
-                <p><MdEmail /> По электронной почте: <a href={contact.emailLink}>semytsvetyk@gmail.com</a></p>
+                <p> <MdEmail /> По электронной почте: <a href={contact.emailLink}>semytsvetyk@gmail.com</a></p>
                 <p><IoLogoWhatsapp /> Наша группа в WhatsApp: <a href={contact.whatsappLink}>присоединиться</a></p>
                 <p><RiInstagramFill /> Мы в Instagram: <a href={contact.instaLink}>@semitsvetik.associacio</a></p>
                 <p><BiLogoTelegram /> Читательский клуб <b>Книгоцветик</b> в Telegram: <a href={contact.telegramLink}>присоединиться</a></p>
