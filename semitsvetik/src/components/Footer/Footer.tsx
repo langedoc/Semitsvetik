f98d96f3da7a77f01c9d2d4fe42f8b149dfea430
yulia <langedoc@gmail.com>
2024-10-17 14:06:37 +0200

    feat: footer content

--- semitsvetik/src/components/Footer/Footer.tsx
+++ semitsvetik/src/components/Footer/Footer.tsx
@@ -1,9 +1,34 @@
 import style from './Footer.module.css';
+import { MdEmail } from "react-icons/md";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { RiInstagramFill } from "react-icons/ri";
+import contacts from '../../data/contactData.json';
 
 export default function Footer() {
+    const contact = contacts[0];
+
     return (
         <footer className={style.container}>
-            <p>Footer</p>
+            <div className={style.content}>
+                <div className={style.name}>
+                    <p>
+                        <b>СЕМИ</b>
+                        <br/>
+                        ЦВЕТИК
+                    </p>
+                </div>
+                <div className={style.contacts}>
+                        <p>Контакты:</p>
+                        <div className={style.contactItems}>
+                            <a href={contact.emailLink}><MdEmail className={style.icon}/></a>
+                            <a href={contact.whatsappLink}><IoLogoWhatsapp className={style.icon}/></a>
+                            <a href={contact.instaLink}><RiInstagramFill className={style.icon}/></a>
+                        </div>
+                </div>
+            </div>
+            <div className={style.copyright}>
+                <p>© 2024 Semitsvetik Associacio. All rights reserved. | Website by <a href="https://yuliakapustinaweb.web.app/">YuliaWebDev</a> </p>
+            </div>
         </footer>
     );
 };
--- semitsvetik/src/pages/Events/Events.tsx
+++ semitsvetik/src/pages/Events/Events.tsx
@@ -1,8 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+
 export default function Events() {
     return (
-        <div>
-            <p>Events calendar coming soon</p>
-            <p>Страница в разработке</p>
-        </div>
+        <>
+            <div>
+                <p>Events calendar coming soon</p>
+                <p>Страница в разработке</p>
+            </div>
+            <Footer />
+        </>
     );
 };
