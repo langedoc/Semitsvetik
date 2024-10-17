213b3ee7d23650d5db38579c4a696f991cab7808
yulia <langedoc@gmail.com>
2024-10-17 11:33:56 +0200

    feat: footer border

--- semitsvetik/src/pages/Root/Root.tsx
+++ semitsvetik/src/pages/Root/Root.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
-import style from './Root.module.css';
 
 export default function Root() {
     return (
         <>
             <Navbar />
-            <main className={style.content}>
-                <Outlet />
-            </main>
+            <Outlet />
         </>
     );
 };
