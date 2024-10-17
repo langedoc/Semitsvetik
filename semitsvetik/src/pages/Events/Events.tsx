4132d4d6196c96073e9ee5a4ca2f9c1730b58113
yulia <langedoc@gmail.com>
2024-10-17 15:50:42 +0200

    feat: events calendar page first design

--- semitsvetik/src/pages/Events/Events.tsx
+++ semitsvetik/src/pages/Events/Events.tsx
@@ -1,13 +1,17 @@
+import style from './Events.module.css';
 import Footer from "../../components/Footer/Footer";
 
 export default function Events() {
     return (
-        <>
-            <div>
-                <p>Events calendar coming soon</p>
+        <div className={style.container}>
+            <div className={style.topInfo}>
+                <h1>Календарь мероприятий</h1>
+            </div>
+            <div className={style.content}>
                 <p>Страница в разработке</p>
+                <p>Coming soon...</p>
             </div>
             <Footer />
-        </>
+        </div>
     );
 };
