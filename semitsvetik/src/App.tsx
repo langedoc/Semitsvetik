f746ae67da6d2291565624b4cb37b3bdc965bbfe
yulia <langedoc@gmail.com>
2024-11-22 10:08:05 +0100

    fix: added index: true to the home route to explicitly mark it as rge index route

--- semitsvetik/src/App.tsx
+++ semitsvetik/src/App.tsx
@@ -12,18 +12,19 @@ import Projects from './pages/Projects/Projects';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <Home />,
+    index: true
   },
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      {path: 'about', element: <About/>},
-      {path: 'school', element: <School/>},
-      {path: 'contact', element: <Contact/>},
-      {path: 'courses', element: <Courses/>},
-      {path: 'events', element: <Events/>},
-      {path: 'projects', element: <Projects/>}
+      {path: '/about', element: <About/>},
+      {path: '/school', element: <School/>},
+      {path: '/contact', element: <Contact/>},
+      {path: '/courses', element: <Courses/>},
+      {path: '/events', element: <Events/>},
+      {path: '/projects', element: <Projects/>}
     ],
   }
 ]);
